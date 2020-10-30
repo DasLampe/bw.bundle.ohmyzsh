91c6d9c721dd9d10af17c52344604b1977caa18c
DasLampe <daslampe@lano-crew.org>
2020-10-30 16:32:56 +0100

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install ohmyz.sh via Bundlewrap
+
+- Get all users where has "zsh" as shell.
+- Clone https://github.com/ohmyzsh/ohmyzsh to /home/{user}/.oh-my-zsh
+- Set {user} as owner on /home/{user}/.oh-my-zsh
+- Disable write-access for world and group on /home/{user}/.oh-my-zsh
---
+++ items.py
@@ -0,0 +1,33 @@
+directories = {}
+git_deploy = {}
+actions = {}
+
+for user in {key: value for (key, value) in node.metadata.get('users', {}).items()
+             if value.get('shell', "")[-3:] == 'zsh'}:
+    dirname = '/home/{}/.oh-my-zsh'.format(user)
+
+    directories[dirname] = {
+        'owner': user,
+    }
+
+    git_deploy[dirname] = {
+        'repo': 'https://github.com/ohmyzsh/ohmyzsh',
+        'rev': 'master',
+        'needs': [
+            'user:{}'.format(user),
+        ]
+    }
+
+    actions['chown_ohmyzsh_dir_for_{}'.format(user)] = {
+        'command': 'chown -R {user} {dirname}'.format(user=user, dirname=dirname),
+        'needs': [
+            'git_deploy:{}'.format(dirname)
+        ]
+    }
+
+    actions['chmod_ohmyzsh_for_{}'.format(user)] = {
+        'command': 'chmod -R g-w,o-w {}'.format(dirname),
+        'needs': [
+            'git_deploy:{}'.format(dirname)
+        ]
+    }
